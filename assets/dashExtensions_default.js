9f157c5034809747e7bbbb5af7b336f17e869b6f
doromboziandras32 <40298967+doromboziandras32@users.noreply.github.com>
2022-03-26 20:24:52 +0100

    event filtering added. but probably needs some refactor since doesnt show all the points

---
+++ assets/dashExtensions_default.js
@@ -0,0 +1,46 @@
+window.dashExtensions = Object.assign({}, window.dashExtensions, {
+    default: {
+        function0: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function1: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function2: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function3: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function4: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function5: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function6: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function7: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function8: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function9: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function10: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function11: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function12: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        },
+        function13: function(feature, context) {
+            return context.props.hideout.includes(feature.properties.trace_name);
+        }
+    }
+});
