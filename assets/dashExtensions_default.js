a1fb34b1dd2218da2d95c9efa0f8325577b86a35
doromboziandras32 <40298967+doromboziandras32@users.noreply.github.com>
2022-03-27 16:35:56 +0200

    checked in debug run, backend filtering, map reloading works fine, next step to do some color coding based on magnitudes

--- assets/dashExtensions_default.js
+++ assets/dashExtensions_default.js
@@ -2,9 +2,6 @@ window.dashExtensions = Object.assign({}, window.dashExtensions, {
     default: {
         function0: function(feature, context) {
             return context.props.hideout.includes(feature.properties.trace_name);
-        },
-        function1: function(feature, context) {
-            return context.props.hideout.includes(feature.properties.trace_name);
         }
     }
 });
