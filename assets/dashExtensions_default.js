5326772ce1d1721cfc0f94c95856c2bba461dadf
doromboziandras32 <40298967+doromboziandras32@users.noreply.github.com>
2022-04-10 14:23:13 +0200

    some files removed to go under quota

--- assets/dashExtensions_default.js
+++ assets/dashExtensions_default.js
@@ -503,6 +503,27 @@ window.dashExtensions = Object.assign({}, window.dashExtensions, {
             return L.marker(latlng, {
                 icon: antenna
             });
+        },
+        function48: function(feature, latlng, context) {
+            const {
+                min,
+                max,
+                colorscale,
+                circleOptions,
+                colorProp
+            } = context.props.hideout;
+            const csc = chroma.scale(colorscale).domain([min, max]); // chroma lib to construct colorscale
+            circleOptions.fillColor = csc(feature.properties[colorProp]); // set color based on color prop.
+            return L.circleMarker(latlng, circleOptions); // sender a simple circle marker.
+        },
+        function49: function(feature, latlng) {
+            const antenna = L.icon({
+                iconUrl: `/static/antenna_img.png`,
+                iconSize: [24, 24]
+            });
+            return L.marker(latlng, {
+                icon: antenna
+            });
         }
     }
 });
